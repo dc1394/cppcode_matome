bd3142ff89d952b7df2ae3631eec4db0a4899a2c
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2024-04-23 19:11:44 +0900

    Exclude a specific file from the test suite.- Skip "Twitter_240420_2.cpp" from testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,12 +6,16 @@ cppfiles = filter(readdir("../", join=true)) do f
 end
 
 for fname in cppfiles
+	# omp.h
+	basename(fname) == "Twitter_240420_2.cpp" && continue
+
 	@testset "$(fname)" begin
 		mktempdir() do d
-			exec = joinpath(d, first(splitext(fname)))
-			run(`g++ -O2 -std=c++17 -DUNIT_TEST $(fname) -o $(exec)`)
+			exec = joinpath(d, basename(first(splitext(fname))))
+			run(`g++ -O2 -Xpreprocessor -fopenmp -std=c++20 -DUNIT_TEST $(fname) -o $(exec)`)
 			o = read(`$(exec)`, String)
-			@test_reference "$(basename(fname)).txt" o
+			reftxt = "$(first(splitext(basename(fname)))).txt"
+			@test_reference reftxt o
 		end
 	end
 end
