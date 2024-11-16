5f7c5768653927e4a0ae0d8c4620877187a7738a
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2024-11-16 15:40:16 +0900

    Update logging

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,14 +11,13 @@ for fname in cppfiles
 	basename(fname) == "Twitter_240430.cpp" && continue
 
 	# too long to run
-	basename(fname) == "Twitter_240507_2.cpp" && continue
-	# debug
-	basename(fname) == "Twitter_240505.cpp" && continue
-
+	basename(fname) == "Twitter_240316_2.cpp" && continue
+	basename(fname) == "Twitter_240507_3.cpp" && continue
 	@testset "$(fname)" begin
 		mktempdir() do d
+			@info "Testing $(fname)"
 			exec = joinpath(d, basename(first(splitext(fname))))
-			run(`g++ -O2 -std=c++20 -DSAVE_RESULT=0 -DTEST $(fname) -o $(exec)`)
+			run(`g++ -O2 -std=c++20 -DTEST $(fname) -o $(exec)`)
 			o = read(`$(exec)`, String)
 			reftxt = "$(first(splitext(basename(fname)))).txt"
 			@test_reference reftxt o
